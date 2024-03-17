6beca0e54b9a7a2654a91dfa38c13165cee1545a
9walnut <kguho9202@gmail.com>
2024-03-17 18:16:30 +0900

    ✨ Create message module
    create message module

--- src/auth/guard/auth.guard.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-@Injectable()
-export class AuthGuard extends NestAuthGuard('jwt') {
-  canActivate(context: ExecutionContext): any {
-    return super.canActivate(context);
-  }
-}
