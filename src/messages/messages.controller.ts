db3a0a619740de67a7ed3ccda30547fac84cb904
9walnut <kguho9202@gmail.com>
2024-03-20 21:27:55 +0900

    ✨ Add dto & mod controller
    add dto and modify controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
   listMessages() {}
 
+  // 타입 정보가 js에도 존재?
   @Post()
-  createMessages() {}
+  createMessages(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
 
   @Get('/:id')
-  getMessage() {}
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
 }
