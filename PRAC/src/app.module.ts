4c2a2ec813d4d46c7f1768d5f77cf9c78545c89e
9walnut <kguho9202@gmail.com>
2024-03-23 10:48:50 +0900

    ✨ Add entity
    add entity

--- PRAC/src/app.module.ts
+++ PRAC/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User, Report],
       synchronize: true,
     }),
   ],
