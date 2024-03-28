0a4865f583bfd3d39793a8b4de90839b7059d789
9walnut <kguho9202@gmail.com>
2024-03-28 21:12:32 +0900

    ✨ Add auth signup
    add auth signup

--- PRAC/src/users/auth.service.ts
+++ PRAC/src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrpyt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrpyt);
 
 @Injectable()
 export class AuthService {
@@ -13,10 +17,20 @@ export class AuthService {
     }
 
     // 해시 사용
+    // 솔트 만들기
+    const salt = randomBytes(8).toString('hex');
+
+    // 해시로 만들기
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    // 해시 + 솔트
+    const result = salt + '.' + hash.toString('hex');
 
     // 사용자 생성
+    const user = await this.usersService.create(email, result);
 
     // 사용자 반환
+    return user;
   }
 
   signin() {}
